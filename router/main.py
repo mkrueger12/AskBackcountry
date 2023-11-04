11758bd44be8fab43b5a03110560007a93db723f
max <max@kruegers.com>
2023-11-04 09:36:23 -0600

    feat: create class

--- router/main.py
+++ router/main.py
@@ -1,52 +1,89 @@
 import os
+import dotenv
 import openai
 from google.cloud import bigquery
 
-openai.api_key = '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'
+dotenv.load_dotenv('utils/.env')
+
+openai.api_key = os.getenv('OPENAI_KEY')
 
 
-def get_query(question):
-    system_content = '''You are a helpful BigQuery SQL assistant. Write a BigQuery SQL query that will answer the user question below. If you are unable to determine a value for the query ask for more information. /
-    <Available columns: Date (yyyy-mm-dd), latitude, longitude, snow_depth (do not use SUM()), new_snow (new snow since yesterday), elevation, state (state code like 'IL'  for Illinois, county, station_name>
-    <Available tables: `avalanche-analytics-project.historical_raw.snow-depth`>
-    If using the county and state columns, be sure that they are correct before returning an answer. Do not include a value if you do not know what the correct value is.
-    do not assume anything in the query.
-    Return only the SQL query.'''
+class SnowDepthQuestion:
 
-    completion = openai.ChatCompletion.create(
-      model="gpt-4",
-      messages=[
-        {"role": "system", "content": system_content},
-        {"role": "user", "content": question}
-      ]
-    )
-
-    return completion.choices[0].message['content']
+    def __init__(self):
+        pass
 
 
-def run_bigquery_query(sql_query):
-    # Initialize a BigQuery client
-    client = bigquery.Client(project='avalanche-analytics-project')
+    def get_query(self, question):
+        system_content = '''You are a helpful BigQuery SQL assistant. Write a BigQuery SQL query that will answer the user question below. If you are unable to determine a value for the query ask for more information. /
+        <Available columns: Date (yyyy-mm-dd), latitude, longitude, snow_depth (do not use SUM()), new_snow (new snow since yesterday), elevation, state (state code like 'IL'  for Illinois, county (administrative subdivision of a state), station_name>
+        <Available tables: `avalanche-analytics-project.historical_raw.snow-depth`>
+        If using the county and state columns, be sure that they are correct before returning an answer. Do not include a value if you do not know what the correct value is.
+        do not assume anything in the query.
+        Return only the SQL query.'''
 
-    try:
-        # Perform a query.
-        QUERY = sql_query
-        query_job = client.query(QUERY)  # API request
-        rows = query_job.result()  # Waits for query to finish
-        result_dicts = [dict(row.items()) for row in rows]
+        completion = openai.ChatCompletion.create(
+          model="gpt-4",
+          messages=[
+            {"role": "system", "content": system_content},
+            {"role": "user", "content": question}
+          ]
+        )
 
-        # Return the result
-        return result_dicts
-    except Exception as e:
-        # Handle exceptions, you might want to log the error or raise it again
-        print(f"Error: {e}")
-        return None
+        return completion.choices[0].message['content']
+
+
+    def run_bigquery_query(self, sql_query):
+        # Initialize a BigQuery client
+        client = bigquery.Client(project='avalanche-analytics-project')
+
+        try:
+            # Perform a query.
+            QUERY = sql_query
+            query_job = client.query(QUERY)  # API request
+            rows = query_job.result()  # Waits for query to finish
+            result_dicts = [dict(row.items()) for row in rows]
+
+            # Return the result
+            return result_dicts
+        except Exception as e:
+            # Handle exceptions, you might want to log the error or raise it again
+            print(f"Error: {e}")
+            return None
+
+
+def get_response(self, data, question):
+        system_content = ('You are a helpful assistant. Answer the user question based on the context. '
+                          'If you are unable to determine a value for the query ask for more information.'
+                          f'<context>: {data}')
+
+        completion = openai.ChatCompletion.create(
+          model="gpt-3.5-turbo",
+          messages=[
+            {"role": "system", "content": system_content},
+            {"role": "user", "content": question}
+          ]
+        )
+
+        return completion.choices[0].message['content']
+
+
+###############################
+
+user_question = SnowDepthQuestion()
 
 question = '<Question: Which snotel site near Vail, CO received the most snow since last Monday?>'
 
-query = get_query(question)
+query = user_question.get_query(question)
 print(query)
 
-result = run_bigquery_query(query)
+result = user_question.run_bigquery_query(query)
 print(result)
+
+if len(result) > 0:
+    response = get_response([result, query], question)
+    print(response)
+
+else:
+    print("Sorry, I don't know the answer to that question. Try provide more detail.")
 
