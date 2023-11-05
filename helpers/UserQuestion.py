4623d4b2b7cb6b9f6f6de7a7f1211394cccca63b
max <max@kruegers.com>
2023-11-04 18:11:53 -0600

    feat: new class

--- helpers/UserQuestion.py
+++ helpers/UserQuestion.py
@@ -8,13 +8,12 @@ dotenv.load_dotenv('utils/.env')
 openai.api_key = os.getenv('OPENAI_KEY')
 
 
-class SnowDepthQuestion:
+class UserQuestion:
 
-    def __init__(self):
-        pass
+    def __init__(self, question):
+        self.question = question
 
-
-    def get_query(self, question):
+    def get_snow_depth_sql(self):
         system_content = '''You are a helpful BigQuery SQL assistant. Write a BigQuery SQL query that will answer the user question below. If you are unable to determine a value for the query ask for more information. /
         <Available columns: Date (yyyy-mm-dd), latitude, longitude, snow_depth (do not use SUM()), new_snow (new snow since yesterday), elevation, state (state code like 'IL'  for Illinois, county (administrative subdivision of a state), station_name (Vail Mountain)>
         <Available tables: `avalanche-analytics-project.historical_raw.snow-depth`>
@@ -45,19 +44,19 @@ class SnowDepthQuestion:
             presence_penalty=0,
             messages=[
                 {"role": "system", "content": system_content},
-                {"role": "user", "content": question}
+                {"role": "user", "content": self.question}
             ]
         )
 
         return completion.choices[0].message['content']
 
-    def collect_data(self, sql_query):
+    def collect_snow_depth_data(self):
         # Initialize a BigQuery client
         client = bigquery.Client(project='avalanche-analytics-project')
 
         try:
             # Perform a query.
-            QUERY = sql_query
+            QUERY = self.get_snow_depth_sql()
             query_job = client.query(QUERY)  # API request
             rows = query_job.result()  # Waits for query to finish
             result_dicts = [dict(row.items()) for row in rows]
@@ -68,41 +67,3 @@ class SnowDepthQuestion:
             # Handle exceptions, you might want to log the error or raise it again
             print(f"Error: {e}")
             return None
-
-    def get_response(self, data, question):
-        system_content = ('You are a helpful assistant. Answer the user question based on the context. '
-                          f'<context>: {data}'
-                          f'If you cannot answer the question, ask for more information. ')
-
-        completion = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            temperature=0.3,
-            messages=[
-                {"role": "system", "content": system_content},
-                {"role": "user", "content": question}
-            ]
-        )
-
-        return completion.choices[0].message['content']
-
-    def run(self, question):
-        # Get the SQL query
-        sql_query = self.get_query(question)
-
-        # Run the query
-        results = self.run_bigquery_query(sql_query)
-
-        if results is not None and len(results) > 0:
-
-            answer = self.get_response(data=[results, sql_query], question=question)
-
-            return answer
-
-        else:
-            return ('Sorry, I could not find any results for your query.'
-                    'I can be picky. Try to be more specific.')
-
-
-##############
-snow_depth = SnowDepthQuestion()
-data_a = snow_depth.run('How much new snow has fallen in the last 24 hours in Eagle County?')
---
+++ router/main.py
@@ -0,0 +1,60 @@
+import openai
+import json
+import dotenv
+import os
+
+dotenv.load_dotenv('utils/.env')
+
+openai.api_key = os.getenv('OPENAI_KEY')
+
+def GPT_QB(user_query):
+
+    ''' Determines which function should be called based on the user's query.'''
+
+    messages = [{"role": "user", "content": user_query}]
+    functions = [
+        {
+            "name": "get_query",
+            "description": "Generates a SQL query for Snow Depth and Snow Fall data based on the user's question.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "user_question": {
+                        "type": "string",
+                        "description": "How much new snow has fallen in the last 24 hours in Eagle County?",
+                    },
+                },
+                "required": ["user_question"],
+            },
+        },
+        {
+            "name": "get_query_hunting",
+            "description": "Generates a SQL query for hunting related data based on the user's question.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "user_question": {
+                        "type": "string",
+                        "description": "What was the archery elk success rate for GMU 49?",
+                    },
+                },
+                "required": ["user_question"],
+            },
+        }
+    ]
+
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
+        temperature=0,
+        max_tokens=512,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+        messages=messages,
+        functions=functions,
+    )
+
+    return response["choices"][0]["message"]["function_call"]
+
+
+response = GPT_QB("How many tags were issued game management unit 23?")
