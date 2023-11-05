343ae885c170a5e26ec2bec32ca55d34a91f3f7f
max <max@kruegers.com>
2023-11-05 10:02:40 -0700

    prompt: add one shot example

--- helpers/UserQuestion.py
+++ helpers/UserQuestion.py
@@ -94,7 +94,7 @@ def location_extraction(question):
     system_content = ('You will be provided with a text, and your task is to extract the county and state from it.'
                       '#### Example ###'
                       'Text: How much snow is at Loveland Pass?'
-                      'Response: {"county": "Clear Creek", "state": "CO"}')
+                      'Response: {"county": "Clear Creek", "state": "CO", "elevation": 11900}')
 
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -126,6 +126,23 @@ def snow_depth_sql(question):
                             Date DATE NULLABLE <yyyy-mm-dd>,
                             snow_depth FLOAT <do not use SUM()>,
                             new_snow FLOAT <inches, only SUM() when GROUP BY station_name>);
+                            
+                            <example>
+                            
+                            user question: How much snow is at Loveland Pass? Additional Context:{"county": "Clear Creek", "state": "CO", "elevation": 11900}
+                            Response:   WITH LatestDate AS (
+                                                          SELECT MAX(Date) AS max_date
+                                                          FROM `avalanche-analytics-project.historical_raw.snow-depth`
+                                                          WHERE county = 'Clear Creek' AND state = 'CO' AND elevation > 9900
+                                                        )
+                                                        
+                                                        SELECT AVG(snow_depth) AS average_snow_depth
+                                                        FROM `avalanche-analytics-project.historical_raw.snow-depth` AS s
+                                                        JOIN LatestDate AS ld
+                                                        ON s.Date = ld.max_date
+                                                        WHERE s.county = 'Clear Creek' AND s.state = 'CO' AND s.elevation > 9900;
+                                                                    
+                            <example>
 
                             Use Google Standard SQL.
                             Return only the SQL query.
